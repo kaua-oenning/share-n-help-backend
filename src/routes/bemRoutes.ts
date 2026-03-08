a3860963c4f95e9cbbe32180f73acd6445fe433f
kauã <kkauaok@gmail.com>
2026-03-08 20:59:18 -0300

    adicionado configuração do prisma

--- src/routes/bemRoutes.ts
+++ src/routes/bemRoutes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from "fastify";
+import { prisma } from "../database/prisma";
 
 export default async function bemRoutes(app: FastifyInstance) {
-
-    app.get
+  app.get("/bens", async (request, reply) => {
+    const bens = await prisma.donation.findMany();
+    return reply.send(bens);
+  });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import bemRoutes from "./routes/bemRoutes";
 
 const app = Fastify({
   logger: true,
 });
 
-app.register(bemRoutes);
-
 async function start() {
+  await app.register(cors, {
+    origin: true,
+  });
+
+  app.register(bemRoutes);
+
   try {
     await app.listen({
       port: 3000,
